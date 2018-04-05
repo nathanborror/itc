d6d866d15ae8c8aa8533c0b92d89c15dd8235d63
Nathan Borror <nathan@nathanborror.com>
2018-04-04 17:15:31 -0700

    Adding command to add a tester

--- cmd/testflight.go
+++ cmd/testflight.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/nathanborror/itc/itunes"
+
 	"github.com/spf13/cobra"
 )
 
@@ -44,6 +46,11 @@ func init() {
 	testflightCmd.AddCommand(testersCmd)
 
 	testersCmd.AddCommand(testersListCmd)
+
+	testersCreateCmd.Flags().StringVar(&testerEmail, "email", "", "Tester email")
+	testersCreateCmd.Flags().StringVar(&testerFirstName, "first-name", "", "Tester first name")
+	testersCreateCmd.Flags().StringVar(&testerLastName, "last-name", "", "Tester last name")
+	testersCreateCmd.Flags().StringVar(&testerGroupID, "group", "", "Tester group ID")
 	testersCmd.AddCommand(testersCreateCmd)
 }
 
@@ -53,6 +60,15 @@ func testersList(cmd *cobra.Command, args []string) {
 	fmt.Printf(prettyPrint(testers))
 }
 
+var (
+	testerEmail     string
+	testerFirstName string
+	testerLastName  string
+	testerGroupID   string
+)
+
 func testersCreate(cmd *cobra.Command, args []string) {
-	fmt.Println("testerCreate: Not Implemented")
+	tester := itunes.CreateTester{Email: testerEmail, FirstName: testerFirstName, LastName: testerLastName}
+	err := client.TesterCreate([]itunes.CreateTester{tester}, providerID, appID, testerGroupID)
+	checkErr(err)
 }
